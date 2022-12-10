a90732d71874afdab808fe9de60cad08af735314
02b2 <revwrapz@hotmail.com>
2022-12-10 13:07:42 +0000

    Changes to be committed: new file:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/components/Navbar.js 	new file:   src/components/pages/About.js 	new file:   src/components/pages/WalkThrough.js 	modified:   src/index.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Hero'
+import Navbar from '../Navbar'
+
+function About() {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        </div>
+  )
+}
+
+export default About
